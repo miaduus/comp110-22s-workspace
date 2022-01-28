b363a32dac945183298a94a1250bd992cbc01726
Mia Duus <mduus@unc.edu>
2022-01-28 09:43:19 -0500

    lesson 10 and 11

---
+++ lessons/emoji_lesson11.py
@@ -0,0 +1,13 @@
+"""Lesson 11 on advanced string concepts."""
+
+print(ord("A"))
+print("Duke" < "UNC")
+print(chr(65))
+print(hex(110))
+
+print("The \U0001F920 rides a \U0001F40E!")
+
+print("The computer said, \"Hello, world.\"")
+
+course: int = 110
+print(f"I am in COMP{ course } right now!")
