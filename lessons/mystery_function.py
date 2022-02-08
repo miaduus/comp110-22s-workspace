547f7b5895470a513a64182f1fc50b32fa933fb7
Mia Duus <mduus@unc.edu>
2022-02-08 18:46:54 -0500

    practice for quiz1

---
+++ lessons/mystery_function.py
@@ -0,0 +1,14 @@
+"""Example of mystery function from class."""
+
+
+def mystery(n: int) -> str:
+    """A useless function."""
+    i: int = 0
+    while i < n:
+        if i % 2 == 1:
+            return "ooh"
+        i += 1
+    return "ahh"
+
+
+print(mystery(4))
